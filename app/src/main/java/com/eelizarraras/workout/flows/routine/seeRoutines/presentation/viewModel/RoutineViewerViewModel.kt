a89f75caefc10f2dd152e1e46f419ab253c32dc6
Edgar Elizarraras <edgar.var@hotmail.com>
2026-06-12 11:36:06 -0600

    Add navigation
    - Add navigation to create a routine.
    - Add template to handle effects.

--- app/src/main/java/com/eelizarraras/workout/flows/routine/seeRoutines/presentation/viewModel/RoutineViewerViewModel.kt
+++ app/src/main/java/com/eelizarraras/workout/flows/routine/seeRoutines/presentation/viewModel/RoutineViewerViewModel.kt
@@ -1,6 +1,8 @@
 package com.eelizarraras.workout.flows.routine.seeRoutines.presentation.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.eelizarraras.workout.core.domine.model.Screen
 import com.eelizarraras.workout.flows.routine.seeRoutines.model.RoutineViewerEffect
 import com.eelizarraras.workout.flows.routine.seeRoutines.model.RoutineViewerEvent
 import com.eelizarraras.workout.flows.routine.seeRoutines.model.RoutineViewerState
@@ -8,6 +10,7 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import org.koin.core.annotation.KoinViewModel
 
 @KoinViewModel
@@ -22,7 +25,20 @@ class RoutineViewerViewModel(
     val uiEffect = _uiEffect.asSharedFlow()
 
     fun handleEvent(event: RoutineViewerEvent) {
+        when(event) {
+            is RoutineViewerEvent.AddRoutine -> {
+                navigateTo(event.screen)
+            }
+            RoutineViewerEvent.PlayRoutine -> {
+                //TODO
+            }
+        }
+    }
 
+    private fun navigateTo(screen: Screen) {
+        viewModelScope.launch {
+            _uiEffect.emit(RoutineViewerEffect.OnNavigateTo(screen))
+        }
     }
 
 }
